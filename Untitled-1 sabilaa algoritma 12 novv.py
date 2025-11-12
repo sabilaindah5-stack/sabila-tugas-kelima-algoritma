1cf44053f54d933880a2d3c84a4410b058d2b3f5
sabilaindah5-stack <sabilaindah5@gmail.com>
2025-11-12 10:37:18 +0700

    Add files via upload

---
+++ Untitled-1 sabilaa algoritma 12 novv.py
@@ -0,0 +1,13 @@
+nilai = int(input('masukkan nilai: '))
+usia = int(input('masukkan usia: '))
+
+if nilai >= 75:
+    if (usia < 15):
+        print('Selamat adek, kamu lulus!')
+    else:
+       print('Selamat kakak, kamu lulus!')
+else:
+    if (usia < 15):
+        print('Mohon maaf dek, coba lagi ya!')
+    else:
+        print('Mohon maaf kak, coba lagi ya!')
