592e257bc786a929363d9bf238b42c6b64ac4204
sabilaindah5-stack <sabilaindah5@gmail.com>
2025-11-12 10:38:13 +0700

    Add files via upload

---
+++ a = 5 sabilaaaa.py
@@ -0,0 +1,14 @@
+a = 5
+b = 5
+nama_a = 'sabila'
+nama_b = 'sabila'
+
+# output True
+print('a is b:', a is b)
+# output False
+print ('a is not b:', a is not b)
+
+# output True
+print ('nama_a is name_b:', nama_a is nama_b)
+# output False
+print('nama_a is not nama_b:', nama_a is not nama_b)')
